5f95eae661229170a83e3921421ff18eb0f5b87a
Felix Laurent <felixlaurent097@gmail.com>
2024-05-27 14:35:10 +0700

    Initial Commit

---
+++ ReTrack Watch App/ReTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReTrackApp.swift
+//  ReTrack Watch App
+//
+//  Created by Felix Laurent on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ReTrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
